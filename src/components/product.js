b77d356eb0bfb9813d0b9b00a7a697a4b920de05
Markusstr <markus.strandman@gmail.com>
2022-01-16 20:35:43 +0200

    Changed product list styling

--- src/components/product.js
+++ src/components/product.js
@@ -6,10 +6,12 @@ export default function Product(props) {
 
 
     const onClickMinus = (product) => {
-        props.setProductList(
-            props.productList.map(item =>
-                item.productName === product.productName ? {...item, productChosenAmount: product.productChosenAmount - 1} : item
-        ));
+        if (product.productChosenAmount > 0) {
+            props.setProductList(
+                props.productList.map(item =>
+                    item.productName === product.productName ? {...item, productChosenAmount: product.productChosenAmount - 1} : item
+            ));
+        }
     }
 
     const onClickPlus = (product) => {
@@ -21,19 +23,23 @@ export default function Product(props) {
 
     return (
         <div className="product-container">
-            {props.productList.map(product => (
-            <div className="product-info">
-                <div className="product-name">
-                    <h2>{product.productName}</h2>
-                </div>
-                <div className="product-amount">
-                    <button type="button" onClick={() => onClickMinus(product)}>-</button>
-                    <p>{product.productChosenAmount} kpl</p>
-                    <button type="button" onClick={() => onClickPlus(product)}>+</button>
-                </div>
+            <ul>
+                {props.productList.map(product => (
+                <li key={product.productName} className="product-info">
+                    <div className="product-name">
+                        <h2>{product.productName}</h2>
+                    </div>
+                    <div className="product-amount">
+                        <button type="button" onClick={() => onClickMinus(product)}>-</button>
+                        <p>{product.productChosenAmount} kpl</p>
+                        <button type="button" onClick={() => onClickPlus(product)}>+</button>
+                    </div>
+                </li>
+                ))}
+            </ul>
+            <div id="product-confirm-button">
+                <button onClick={props.confirmProducts}>Vahvista tuotteet</button>
             </div>
-            ))}
-            <button onClick={props.confirmProducts}>Vahvista tuotteet</button>
         </div>
         
     )
