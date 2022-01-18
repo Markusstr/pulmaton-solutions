56e94469e264aae149bf770b3b971619acdf2e41
Markusstr <markus.strandman@gmail.com>
2022-01-18 17:07:22 +0200

    Fetch information from JSON files

--- src/components/main.js
+++ src/components/main.js
@@ -7,168 +7,45 @@ import Component from "./component";
 
 export default function Main() {
 
-    const [productList, setProductList] = useState([
-        {
-            productName: "Vitriini",
-            productChosenAmount: 0,
-            productComponents: [
-                {
-                    componentName: "Lasiovi",
-                    componentAmount: 2
-                },
-                {
-                    componentName: "Vanerilevy",
-                    componentAmount: 1
-                },
-                {
-                    componentName: "Hyllylevy",
-                    componentAmount: 1
-                },
-                {
-                    componentName: "Ruuvi",
-                    componentAmount: 16
-                },
-                {
-                    componentName: "Vedin",
-                    componentAmount: 2
-                }
-            ]
-        },
-        {
-            productName: "Tuoli",
-            productChosenAmount: 0,
-            productComponents: [
-                {
-                    componentName: "Kangaspala",
-                    componentAmount: 1
-                },
-                {
-                    componentName: "Ruuvi",
-                    componentAmount: 8
-                },
-                {
-                    componentName: "Koivulankku",
-                    componentAmount: 3
-                }
-            ]
-        },
-        {
-            productName: "Sivupöytä",
-            productChosenAmount: 0,
-            productComponents: [
-                {
-                    componentName: "Koivulankku",
-                    componentAmount: 1.4
-                },
-                {
-                    componentName: "Pieni mäntylevy",
-                    componentAmount: 1
-                },
-                {
-                    componentName: "Ruuvi",
-                    componentAmount: 4
-                },
-                {
-                    componentName: "Vedin",
-                    componentAmount: 1
-                }
-
-            ]
-        },
-        {
-            productName: "Kaappi",
-            productChosenAmount: 0,
-            productComponents: [
-                {
-                    componentName: "Iso mäntylevy",
-                    componentAmount: 1
-                },
-                {
-                    componentName: "Vanerilevy",
-                    componentAmount: 1
-                },
-                {
-                    componentName: "Vedin",
-                    componentAmount: 2
-                },
-                {
-                    componentName: "Ruuvi",
-                    componentAmount: 20
-                },
-                {
-                    componentName: "Hyllylevy",
-                    componentAmount: 2
-                }
-            ]
-        }
-    ]);
-
-    const [componentLocationList, setComponentLocationList] = useState([
-        {
-            componentName: "Hyllylevy",
-            componentChosenAmount: 0,
-            componentLocation: "C-10",
-            componentUnit: "kpl"
-        },
-        {
-            componentName: "Pieni mäntylevy",
-            componentChosenAmount: 0,
-            componentLocation: "B-2",
-            componentUnit: "kpl"
-        },
-        {
-            componentName: "Iso mäntylevy",
-            componentChosenAmount: 0,
-            componentLocation: "B-1",
-            componentUnit: "kpl"
-        },
-        {
-            componentName: "Koivulankku",
-            componentChosenAmount: 0,
-            componentLocation: "D-4",
-            componentUnit: "m"
-        },
-        {
-            componentName: "Vanerilevy",
-            componentChosenAmount: 0,
-            componentLocation: "D-1",
-            componentUnit: "kpl"
-        },
-        {
-            componentName: "Kangaspala",
-            componentChosenAmount: 0,
-            componentLocation: "A-4",
-            componentUnit: "kpl"
-        },
-        {
-            componentName: "Lasiovi",
-            componentChosenAmount: 0,
-            componentLocation: "E-1",
-            componentUnit: "kpl"
-        },
-        {
-            componentName: "Ruuvi",
-            componentChosenAmount: 0,
-            componentLocation: "A-2",
-            componentUnit: "kpl"
-        },
-        {
-            componentName: "Vedin",
-            componentChosenAmount: 0,
-            componentLocation: "A-3",
-            componentUnit: "kpl"
-        },
-    ]);
-
-
+    const [productList, setProductList] = useState();
+    const [componentLocationList, setComponentLocationList] = useState();
 
 
     useEffect(() => {
-        //TODO Fetch product and component information from JSON files
+        //Fetch product and component information from JSON files
+        const fetchProducts = async() => {
+            try {
+                let response = await fetch("products.json");
+                if (response.status === 200) {
+                    let jsonData = await response.json();
+                    setProductList(jsonData);
+                }
+            }
+            catch(err) {
+                console.log(err);
+            }
+        }
+
+        const fetchComponents = async() => {
+            try {
+                let response = await fetch("components.json");
+                if (response.status === 200) {
+                    let jsonData = await response.json();
+                    setComponentLocationList(jsonData);
+                }
+            }
+            catch(err) {
+                console.log(err);
+            }
+        }
+
+        fetchProducts();
+        fetchComponents();
     },[]);
 
 
     const confirmProducts = () => {
+        //Change the chosen product amounts in the componentLocationList
         let totalList = [...componentLocationList]
         totalList = totalList.map(item => ({...item, componentChosenAmount: 0}))
 
@@ -181,6 +58,7 @@ export default function Main() {
                 });
             }
         });
+        //Sort the components by location letter and then by number
         totalList.sort((a,b) => {
             let locationA = a.componentLocation.split("-");
             let locationB = b.componentLocation.split("-");
